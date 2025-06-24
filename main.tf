3cf6c9dd588c49589e5d18c52e87741332fc5973
EYHEREGARAY <yann.eyheregaray@ynov.com>
2025-06-24 23:19:41 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+# Groupes de ressources
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+##module "network" {
+##  source = "./modules/network"
+##  resource_group_name = azurerm_resource_group.rg.name
+##  location            = var.location
+## }
+
+module "acr" {
+  source = "./modules/acr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  acr_name            = var.acr_name
+}
+
+module "mysql" {
+  source = "./modules/mysql"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  mysql_name          = var.mysql_name
+  admin_username      = var.mysql_admin
+  admin_password      = var.mysql_password
+}
+
+module "webapp" {
+  source = "./modules/webapp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  app_name            = var.webapp_name
+  acr_login_server    = module.acr.login_server
+  acr_username        = module.acr.username
+  acr_password        = module.acr.password
+  mysql_fqdn          = module.mysql.mysql_fqdn
+  mysql_user          = var.mysql_admin
+  mysql_password      = var.mysql_password
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "acr_name" { type = string }
+variable "mysql_name" { type = string }
+variable "mysql_admin" { type = string }
+variable "mysql_password" { type = string }
+variable "webapp_name" { type = string }
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
